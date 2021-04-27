c5dd48b905a1de4e97ddeb526145a832480a504a
Jacqueline <jlee4332@gmail.com>
2021-04-27 16:04:53 -0400

    final

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,8 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter a store name"
+input = gets.chomp
+new_store = Store.create(name: "#{input}")
+
+puts new_store.errors.message

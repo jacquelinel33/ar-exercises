5b696604ded6740e478916e2b65d190bc0e7fe1c
Jacqueline <jlee4332@gmail.com>
2021-04-27 14:45:25 -0400

    exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,4 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
-@store1 = 
+@store1 = Store.find_by(id: '1')
+@store2 = Store.find_by(id: '2')
+
+@store1.update(name: 'Toronto')
+
+puts Store
